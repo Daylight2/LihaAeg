8bcd7aefb377e24c908471f51f6620e2aa7cadc5
Daylight <18598676+Daylight2@users.noreply.github.com>
2023-05-10 16:26:15 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,68 +1,88 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        SwingUtilities.invokeLater(() -> {
+            Gui gui = new Gui();
+            try {
+                gui.createAndShowGui();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
-        System.out.print("Sisesta lihatüüp (veiseliga, sealiha, kanaliha, kanaliha, kalkuniliha): ");
-        String lihaTüüp = scanner.nextLine();
 
-        System.out.print("Sisestage liha kaal (grammides): ");
-        double weight = scanner.nextDouble();
+    public static List<Tellimus> loeTellimused(File failinimi) throws IOException {
+        List<Tellimus> tellimused = new ArrayList<>();
+        InputStream fail = new FileInputStream(failinimi);
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(fail, "UTF-8"))) {
 
-        System.out.print("Sisestage soovitud küpsusaste (rare, medium-rare, medium, medium-well, well-done): ");
-        String küpsetusTase = scanner.next();
+            String rida;
+            int lauaNr = 0;
+            String lihaTüüp;
+            double lihaKaal = 0;
+            String küpsusAste;
+            boolean onLisandiga;
+            String suurus;
 
-        System.out.print("Sisestage ahju temperatuur: ");
-        int temperatuur = scanner.nextInt();
+            while ((rida = bf.readLine()) != null) {
+                String[] tükid = rida.split(";");
+                lauaNr = Integer.parseInt(tükid[0]);
+                lihaTüüp = tükid[1];
+                if (rida.contains("@")) { //lisandiga tellimused
+                    onLisandiga = true;
+                    if (lihaTüüp.equalsIgnoreCase("veiseliha")) { //veiseliha on ainus, millel määrata erinev (ehk failist tulenev) soovitud küpsusastet
+                        suurus = tükid[2];
+                        String[] jupid = tükid[3].split("@");
+                        küpsusAste = jupid[0];
+                    } else { //teiste lihatüüpide (kana, kalkun ja siga) korral võiks soovituslik küpsusaste olla määratud "well-done"
+                        String[] jupid = tükid[2].split("@");
+                        suurus = jupid[0];
+                        küpsusAste = "well-done";
+                    }
+                } else {
+                    onLisandiga = false;
+                    if (lihaTüüp.equalsIgnoreCase("veiseliha")) { //veiseliha on ainus, millel määrata erinev (ehk failist tulenev) soovitud küpsusastet
+                        suurus = tükid[2];
+                        küpsusAste = tükid[3];
+                    } else { //teiste lihatüüpide (kana, kalkun ja siga) korral võiks soovituslik küpsusaste olla määratud "well-done"
+                        suurus = tükid[2];
+                        küpsusAste = "well-done";
+                    }
+                }
 
-        System.out.print("Kas liha on külm? (true/false): ");
-        boolean külmutatud = scanner.nextBoolean();
+                if (suurus.equalsIgnoreCase("väike")) { //kuna küpsetusaja määramisel eristame kaalusid suurem või väiksem 500grammist, siis määrame vastavalt "väike praad" 350g ning "suur praad" 550G
+                    lihaKaal = 350;
+                } else {
+                    lihaKaal = 550;
+                }
 
-        double küpsetusAeg = 0;
-
-        switch (lihaTüüp.toLowerCase()) {
-            case "veiseliha":
-                küpsetusAeg = 30;
-                break;
-            case "sealiha":
-                küpsetusAeg = 35;
-                break;
-            case "kanaliha":
-                küpsetusAeg = 40;
-                break;
-            case "kalkuniliha":
-                küpsetusAeg = 45;
-                break;
-            default:
-                System.out.println("Invalid meat type.");
-                return;
+                Tellimus tellimus = new Tellimus(lauaNr, lihaTüüp, küpsusAste, lihaKaal, onLisandiga); //loome tellimused
+                tellimused.add(tellimus); //lisame tellimused listi
+            }
         }
+        return tellimused;
+    }
 
-        if (küpsetusTase.equalsIgnoreCase("rare")) {
-            küpsetusAeg -= 10;
-        } else if (küpsetusTase.equalsIgnoreCase("medium-rare")) {
-            küpsetusAeg -= 5;
-        } else if (küpsetusTase.equalsIgnoreCase("medium-well")) {
-            küpsetusAeg += 5;
-        } else if (küpsetusTase.equalsIgnoreCase("well-done")) {
-            küpsetusAeg += 10;
+    //laudade kaupa tellimuste lisamine kujutusse (HashMap)
+    public static Map<Integer, List<Tellimus>> laudadeTellimused(List<Tellimus> tellimused) {
+        Map<Integer, List<Tellimus>> lauad = new HashMap<>();
+        for (Tellimus tellimus : tellimused) {
+            int lauaNr = tellimus.getLauaNr();
+            if (lauad.containsKey(lauaNr)) { //kui sellise laua numbriga tellimus juba on, lisame listi
+                List<Tellimus> lauaTellimused = lauad.get(lauaNr);
+                lauaTellimused.add(tellimus);
+                lauad.put(lauaNr, lauaTellimused);
+            } else { //kui sellise laua numbriga tellimust ei olnud, loome uue listi
+                List<Tellimus> lauaTellimused = new ArrayList<>();
+                lauaTellimused.add(tellimus);
+                lauad.put(lauaNr, lauaTellimused);
+            }
         }
-
-        if (temperatuur < 185) {
-            küpsetusAeg += 8;
-        } else if (temperatuur > 215) {
-            küpsetusAeg -= 8;
-        }
-
-        if (külmutatud) {
-            küpsetusAeg += 5;
-        }
-
-        if (weight > 500) {
-            küpsetusAeg = küpsetusAeg * (1 + weight / 1000);
-        }
-
-        System.out.println("Küpsetusaeg: " + küpsetusAeg + " minutit.");
+        return lauad;
     }
 }
